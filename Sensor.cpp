b91ff750050cd3a2a643426e33daaadff927e298
kularos <ian@kularos.com>
2022-01-26 13:09:24 -0500

    Debugging, insignificant commits.

--- Sensor.cpp
+++ Sensor.cpp
@@ -5,15 +5,10 @@
 #include "SerialServo.h"
 #include "Sensor.h"
 
-Sensor::Sensor(uint8_t n){
-    nDim = n;
-    senseVec[nDim];
-    readBuffer[nDim];
-}
 
 byte Sensor::updateSense(void){
     // Attempt to read the sensor
-    senseError = readSense();
+    byte senseError = readSense();
 
     // Manage sensor errors
     if(senseError != NO_ERROR){
